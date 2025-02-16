da2618adc10ee09b605d8e0d213cb7366baf92a7
JallToTea <box4gora@gmail.com>
2025-02-16 21:19:22 +0500

    things.py

--- things.py
+++ things.py
@@ -1,4 +1,22 @@
-# Удиви своих одногруппников!
+#!/usr/bin/python3
 import random
-things = ["мое хобби - это игра на барабанах", "мое любимое блюдо - шоколадный фондан", "мой питомец - серый кот по имени Бонни", "моя любимая книга - Позже от Стивена Кинга"]
-print(random.choice(things))
+import telebot
+from telebot.types import ReactionTypeEmoji
+
+API_TOKEN = ''
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Send a reactions to all messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎", "😳"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+
+@bot.message_reaction_handler(func=lambda message: True)
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+
+
+bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
